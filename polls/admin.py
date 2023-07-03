7b997238a89c3ef4f8ff2c9d2fc596ce393a2d66
Shinhaeng Lee <slee3487@gatech.edu>
2023-07-03 14:39:48 -0400

    Field Separation

--- polls/admin.py
+++ polls/admin.py
@@ -4,9 +4,21 @@ from django.contrib import admin
 
 from polls.models import Question, Choice
 
-# field order 
+
+""" 
+
+field order
 class QuestionAdmin(admin.ModelAdmin):
      fields = ['pub_date', 'question_text']
+
+"""
+# field separation
+
+class QuestionAdmin(admin.ModelAdmin):
+     fieldsets = [
+          ('Question Statement', {'fields': ['question_text']}),
+          ('Date Information', {'fields': ['pub_date']}),
+     ]
 
 admin.site.register(Question, QuestionAdmin)
 admin.site.register(Choice)
