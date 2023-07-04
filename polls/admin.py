ab2f28a3d06fbd9fd49c561cbc43244df64cb6c6
Shinhaeng Lee <slee3487@gatech.edu>
2023-07-04 12:34:53 -0400

    List Column created by using list_display

--- polls/admin.py
+++ polls/admin.py
@@ -34,6 +34,7 @@ class QuestionAdmin(admin.ModelAdmin):
           ('Date Information', {'fields': ['pub_date'], 'classes': ['collapse']}),
      ]
      inlines = [ChoiceInline]  # Choice model class under Question model
+     list_display = ('question_text', 'pub_date')  # list column created
 
 admin.site.register(Question, QuestionAdmin)
 admin.site.register(Choice)
